02b9ff26f1fec8644b48bd699e9b800330be6e93
Daryl McMillan <darylm@gmail.com>
2014-07-30 22:48:31 -0400

    adding basic drawing

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+var surface = document.getElementById("surface");
+var ctx = surface.getContext("2d");
+var selected = {}
+
 var module = angular.module('drawinator', []);
 
 module.controller('ColorPicker',[
   '$scope', function($scope) {
-    
+
     var cid = 0;
     var color = function(value) {
       cid ++;
-      return { id: 'c' + cid, label: 'color ' + cid, value: value };
+      return {
+        id: 'c' + cid,
+        label: 'color ' + cid,
+        value: value
+      };
     };
     
     $scope.colors = [
@@ -26,8 +34,21 @@ module.controller('ColorPicker',[
       color("#CCCCCC")
     ];
     
-    $scope.selected = { color: $scope.colors[0] };
+    $scope.selected = selected;
+    selected.color = $scope.colors[0];
     
     $scope.test = "my test value";
   }
 ]);
+
+function canvasClick(e) {
+  var parts = selected.color.value.match(/[0-9A-F]{2}/g)
+  var x = e.offsetX;
+  var y = e.offsetY;
+  var data = ctx.getImageData(x, y, 1, 1);
+  data.data[0] = parseInt(parts[0],16);
+  data.data[1] = parseInt(parts[1],16);
+  data.data[2] = parseInt(parts[2],16);
+  data.data[3] = 255;
+  ctx.putImageData(data, x, y);
+}
